51d879c1981e1a8a6c4d2b42b5a9732234bdbf12
Aniket Karmarkar <karmarkaraniket@gmail.com>
2018-08-02 11:44:07 -0500

    initial commit number of working days in a month

---
+++ NumberOfWorkingDaysInAMonth/NumberOfWorkingDaysInAMonth/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberOfWorkingDaysInAMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "2018-07";
+        }
+    }
+}

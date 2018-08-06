b61142bbc7c1e085dfd87f1b732880b62879e25c
Aniket Karmarkar <karmarkaraniket@gmail.com>
2018-08-06 11:21:52 -0500

    added conditions to get number of weekend days in a month

--- NumberOfWorkingDaysInAMonth/NumberOfWorkingDaysInAMonth/Program.cs
+++ NumberOfWorkingDaysInAMonth/NumberOfWorkingDaysInAMonth/Program.cs
@@ -6,19 +6,37 @@ namespace NumberOfWorkingDaysInAMonth
     {
         static void Main(string[] args)
         {
-            string input = "2018-07";
-            Console.WriteLine("Test");
+            string input = "2050-02";
             //Get what day of week it is for startdate and for enddate of month
-            
-            DateTime startDateOfMonth = new DateTime(Convert.ToInt32(input.Substring(0, 4)), Convert.ToInt32(input.Substring(4, 2)), 1);
+            int year = Convert.ToInt32(input.Substring(0, 4));
+            int month = Convert.ToInt32(input.Substring(5, 2));
 
-            DateTime endDateOfMonth = new DateTime();
+            DateTime startDateOfMonth = new DateTime(year, month, 1);
+            DateTime endDateOfMonth = new DateTime(year, month, System.DateTime.DaysInMonth(year, month));
+
+            DayOfWeek startDayOfMonth = startDateOfMonth.DayOfWeek;
+            DayOfWeek endDayOfMonth = endDateOfMonth.DayOfWeek;
+
+            Console.WriteLine("startDayOfMonth:" + startDayOfMonth);
+            Console.WriteLine("endDayOfMonth:" + endDayOfMonth);
 
             //if it starts and ends on weekday only 4 weekends
-
-            //if it starts on Friday or weekend and ends between weekend and Tuesday and difference is less than 3 between days then 9 days
-
+            if((startDayOfMonth == DayOfWeek.Monday || startDayOfMonth == DayOfWeek.Tuesday || startDayOfMonth == DayOfWeek.Wednesday || startDayOfMonth == DayOfWeek.Thursday || startDayOfMonth == DayOfWeek.Friday) && (endDayOfMonth == DayOfWeek.Monday || endDayOfMonth == DayOfWeek.Tuesday || endDayOfMonth == DayOfWeek.Wednesday || endDayOfMonth == DayOfWeek.Thursday || endDayOfMonth == DayOfWeek.Friday))
+            {
+                Console.WriteLine("8 weekend days in a month");
+            }
             //if it starts on Saturday and ends on Monday then 10 days to the weekend
+            else if ((startDayOfMonth == DayOfWeek.Friday || startDayOfMonth == DayOfWeek.Saturday) && (endDayOfMonth == DayOfWeek.Monday || endDayOfMonth == DayOfWeek.Sunday))
+            {
+                Console.WriteLine("10 weekend days in a month");
+            }
+            //if it starts on Friday or weekend and ends between weekend and Tuesday and difference is less than 3 between days then 9 days
+            else if((startDayOfMonth == DayOfWeek.Thursday || startDayOfMonth == DayOfWeek.Friday || startDayOfMonth == DayOfWeek.Saturday || startDayOfMonth == DayOfWeek.Sunday) && (endDayOfMonth == DayOfWeek.Saturday || endDayOfMonth == DayOfWeek.Sunday || endDayOfMonth == DayOfWeek.Monday || endDayOfMonth == DayOfWeek.Tuesday))
+            {
+                Console.WriteLine("9 weekend days in a month");
+            }
+
+            Console.ReadLine();
         }
     }
 }
